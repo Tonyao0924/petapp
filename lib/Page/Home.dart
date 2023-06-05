03b669b014f39c6c36d41a785f213aa4294ae8e5
Aven910126 <a1148370488@gmail.com>
2023-06-05 22:14:38 +0800

    petOverview fix

--- lib/Page/Home.dart
+++ lib/Page/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:petapp/Page/petInfo/petInfoPage.dart';
 
 import 'package:petapp/Page/petInsert/petInsertPage.dart';
 import 'package:petapp/Page/petoverview/petOverview.dart';
@@ -75,65 +76,45 @@ class _HomePage extends State<HomePage> {
                           ),
                         ),
                       ),
-                    ),
-                    Center(
-                      child: Container(
-                        margin: EdgeInsets.only(
-                            left: 20, top: 30, right: 0, bottom: 0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context, MaterialPageRoute(builder: (context) => PetOverview()));
-                          },
-                          style: ElevatedButton.styleFrom(
-                            padding: EdgeInsets.zero, // 去除內部填充
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0), // 設置圓角
-                            ),
-                            backgroundColor: Colors.transparent, // 設置背景為透明
-                          ),
-                          child: Ink.image(
-                            image: AssetImage(
-                                'assets/images/petoverview.png'), // 按鈕背景圖片
-                            fit: BoxFit.cover, // 圖片填充方式
-                            child: InkWell(
-                              onTap: () {
+                      Center(
+                          child: Container(
+                            margin: EdgeInsets.only(
+                                left: 20, top: 30, right: 0, bottom: 0),
+                            child: ElevatedButton(
+                              onPressed: () {
                                 Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => PetOverview()),
-                                );
-                                // 按鈕點擊事件處理
+                                    context, MaterialPageRoute(builder: (context) => PetOverview()));
                               },
-                              child: Container(
-                                width: 156, // 按鈕寬度
-                                height: 156, // 按鈕高度
+                              style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.zero, // 去除內部填充
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30.0), // 設置圓角
+                                ),
+                                backgroundColor: Colors.transparent, // 設置背景為透明
                               ),
-                              backgroundColor: Colors.transparent, // 設置背景為透明
-                            ),
-                            child: Ink.image(
-                              image: AssetImage(
-                                  'assets/images/petoverview.png'), // 按鈕背景圖片
-                              fit: BoxFit.cover, // 圖片填充方式
-                              child: InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => PetInfoPage()),
-                                  );
-                                  // 按鈕點擊事件處理
-                                },
-                                child: Container(
-                                  width: 156, // 按鈕寬度
-                                  height: 156, // 按鈕高度
+                              child: Ink.image(
+                                image: AssetImage(
+                                    'assets/images/petoverview.png'), // 按鈕背景圖片
+                                fit: BoxFit.cover, // 圖片填充方式
+                                child: InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => PetOverview()),
+                                    );
+                                    // 按鈕點擊事件處理
+                                  },
+                                  child: Container(
+                                    width: 156, // 按鈕寬度
+                                    height: 156, // 按鈕高度
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ),
-                      )
+                          )
+                      ),
                     ],
-                  ),
+                    ),
                   Center(
                     child: Container(
                       width: 300,
