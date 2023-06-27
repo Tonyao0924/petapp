43823a6ace496b1d271aa55f35652bffb444f005
fan9704 <cxz123499@gmail.com>
2023-06-27 16:00:40 +0800

    Upload Windows/MacOS Project Setting

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,18 @@
 import FlutterMacOS
 import Foundation
 
-import file_selector_macos
+import facebook_auth_desktop
 import firebase_auth
 import firebase_core
 import firebase_messaging
+import flutter_secure_storage_macos
 import rive_common
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
 }
