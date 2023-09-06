10898a21e73c68baa40c3773acb833f8c0f40f9e
PeNDy <b10923024@gemail.yuntech.edu.tw>
2023-09-06 23:34:18 +0800

    Add KibanaTutorial

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,21 @@ import FlutterMacOS
 import Foundation
 
 import facebook_auth_desktop
+import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_messaging
+import flutter_local_notifications
 import flutter_secure_storage_macos
 import rive_common
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
 }
